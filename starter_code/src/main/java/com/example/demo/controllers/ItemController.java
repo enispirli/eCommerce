449683bc76ec66b15e6dcc8f0e9cf6c164832ff3
LAPTOP-8GG4G6OD\enisp <en.ispirli@gmail.com>
2023-05-02 22:09:40 +0300

    Added log info an error for controller.

--- starter_code/src/main/java/com/example/demo/controllers/ItemController.java
+++ starter_code/src/main/java/com/example/demo/controllers/ItemController.java
@@ -1,9 +1,13 @@
 package com.example.demo.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,25 +20,36 @@ import com.example.demo.model.persistence.repositories.ItemRepository;
 @RequestMapping("/api/item")
 public class ItemController {
 
-	@Autowired
-	private ItemRepository itemRepository;
-	
-	@GetMapping
-	public ResponseEntity<List<Item>> getItems() {
-		return ResponseEntity.ok(itemRepository.findAll());
-	}
-	
-	@GetMapping("/{id}")
-	public ResponseEntity<Item> getItemById(@PathVariable Long id) {
-		return ResponseEntity.of(itemRepository.findById(id));
-	}
-	
-	@GetMapping("/name/{name}")
-	public ResponseEntity<List<Item>> getItemsByName(@PathVariable String name) {
-		List<Item> items = itemRepository.findByName(name);
-		return items == null || items.isEmpty() ? ResponseEntity.notFound().build()
-				: ResponseEntity.ok(items);
-			
-	}
-	
+    private static final Logger log = LoggerFactory.getLogger(ItemController.class);
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Item>> getItems() {
+        List<Item> items = itemRepository.findAll();
+        log.info("Get items.Items size: " + items.size());
+        return ResponseEntity.ok(items);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Item> getItemById(@PathVariable Long id) {
+        Optional<Item> itemWrapper = itemRepository.findById(id);
+        if (!itemWrapper.isPresent()) {
+            log.error("Item not found item id" + id);
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(itemWrapper.get());
+    }
+
+    @GetMapping("/name/{name}")
+    public ResponseEntity<List<Item>> getItemsByName(@PathVariable String name) {
+        List<Item> items = itemRepository.findByName(name);
+        if (CollectionUtils.isEmpty(items)) {
+            log.warn("Items is empty by name :" + name);
+            ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(items);
+    }
+
 }
--- starter_code/src/main/java/com/example/demo/controllers/UserController.java
+++ starter_code/src/main/java/com/example/demo/controllers/UserController.java
@@ -19,51 +19,63 @@ import com.example.demo.model.persistence.repositories.CartRepository;
 import com.example.demo.model.persistence.repositories.UserRepository;
 import com.example.demo.model.requests.CreateUserRequest;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
 public class UserController {
 
-	private static final Logger log = LoggerFactory.getLogger(UserController.class);
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
-	private final UserRepository userRepository;
+    private final UserRepository userRepository;
 
-	private final CartRepository cartRepository;
+    private final CartRepository cartRepository;
 
-	private final PasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
 
-	@GetMapping("/id/{id}")
-	public ResponseEntity<User> findById(@PathVariable Long id) {
-		return ResponseEntity.of(userRepository.findById(id));
-	}
+    @GetMapping("/id/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        Optional<User> userWrapper = userRepository.findById(id);
+        if (!userWrapper.isPresent()) {
+            log.error("User not found : userId", id);
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(userWrapper.get());
+    }
 
-	@GetMapping("/{username}")
-	public ResponseEntity<User> findByUserName(@PathVariable String username) {
-		User user = userRepository.findByUsername(username);
-		return user == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(user);
-	}
+    @GetMapping("/{username}")
+    public ResponseEntity<User> findByUserName(@PathVariable String username) {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            log.error("User not found : username", user.getUsername());
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(user);
+    }
 
-	@PostMapping("/create")
-	public ResponseEntity<User> createUser(@RequestBody CreateUserRequest createUserRequest) {
-		User user = new User();
-		user.setUsername(createUserRequest.getUsername());
-		Cart cart = new Cart();
-		cartRepository.save(cart);
-		user.setCart(cart);
+    @PostMapping("/create")
+    public ResponseEntity<User> createUser(@RequestBody CreateUserRequest createUserRequest) {
+        User user = new User();
+        user.setUsername(createUserRequest.getUsername());
+        Cart cart = new Cart();
+        cartRepository.save(cart);
+        user.setCart(cart);
 
-		if (createUserRequest.getPassword().length() < 8){
-			log.error("Password can have at least 8 characters");
-			return ResponseEntity.badRequest().build();
-		}
+        if (createUserRequest.getPassword().length() < 8) {
+            log.error("Password can have at least 8 characters");
+            return ResponseEntity.badRequest().build();
+        }
 
-		if (!createUserRequest.getPassword().equals(createUserRequest.getConfirmPassword())){
-			log.error("Passwords dont match");
-			return ResponseEntity.badRequest().build();
-		}
+        if (!createUserRequest.getPassword().equals(createUserRequest.getConfirmPassword())) {
+            log.error("Passwords dont match");
+            return ResponseEntity.badRequest().build();
+        }
 
-		user.setPassword(passwordEncoder.encode(createUserRequest.getPassword()));
-		userRepository.save(user);
-		return ResponseEntity.ok(user);
-	}
+        user.setPassword(passwordEncoder.encode(createUserRequest.getPassword()));
+        userRepository.save(user);
+        log.info("Created user :" + user.getUsername());
+        return ResponseEntity.ok(user);
+    }
 
 }
